ca0c75cf819afade6c9a4182d2c6060a51b296e1
Your Name <xdeividaspetrulis@gmail.com>
2025-03-01 14:03:34 +0100

    Layout #0.2 - Adjusted the visuals a bit - Can't seem to figure out how to change logo to white when selected

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 
 namespace Sem2Proj.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private int selectedTabIndex;
+    
+    /*
+    [ObservableProperty]
+    private string imageSource = "avares://Sem2Proj/Assets/white-danfoss-logo.png";
+
+    */
     public ScenarioManagerViewModel ScenarioManagerViewModel { get; }
     public AssetManagerViewModel AssetManagerViewModel { get; }
     public OptimizerViewModel OptimizerViewModel { get; }
@@ -16,4 +25,19 @@ public partial class MainWindowViewModel : ViewModelBase
         OptimizerViewModel = new OptimizerViewModel();
         HomeViewModel = new HomeViewModel();
     }
+    /*
+    partial void OnSelectedTabIndexChanged(int oldValue, int newValue)
+    {
+        if (newValue == 3)
+        {
+            ImageSource = "avares://Sem2Proj/Assets/white-danfoss-logo.png";
+            Console.WriteLine($"Image source changed to: {ImageSource}");
+        }
+        else
+        {
+            ImageSource = "avares://Sem2Proj/Assets/black-danfoss-logo.png";
+            Console.WriteLine($"Image source changed to: {ImageSource}");
+        }
+    }
+    */
 }
