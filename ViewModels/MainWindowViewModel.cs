27955f16b274d42ce0645a3a2954376d6814f90d
David <xdeividaspetrulis@gmail.com>
2025-03-12 15:03:41 +0100

    Layout v1.1 - Removed Scenario Manager

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,14 +8,12 @@ namespace Sem2Proj.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     // ViewsModels
-    public ScenarioManagerViewModel ScenarioManagerViewModel { get; }
     public AssetManagerViewModel AssetManagerViewModel { get; }
     public OptimizerViewModel OptimizerViewModel { get; }
     public HomeViewModel HomeViewModel { get; }
 
     public MainWindowViewModel()
     {
-        ScenarioManagerViewModel = new ScenarioManagerViewModel();
         AssetManagerViewModel = new AssetManagerViewModel();
         OptimizerViewModel = new OptimizerViewModel();
         HomeViewModel = new HomeViewModel();
