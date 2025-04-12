49f26c48559310f67ed2867e87b3bea8768f7fbc
David <xdeividaspetrulis@gmail.com>
2025-04-12 16:09:24 +0200

    Update loading screen

---
+++ ViewModels/LoadingWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Media.Imaging;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using Sem2Proj.Models;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Sem2Proj.ViewModels;
+
+public partial class LoadingWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private int progress;
+    [ObservableProperty] private string loadingText = "Starting...";
+    private List<(string message, int steps, int delay)> messages =
+    [
+    ("Starting application...", 10, 10),
+
+    ("Initializing AppData...", 15, 20),
+    ("Loading configuration files...", 20, 20),
+    ("Applying user settings...", 15, 20),
+
+    ("Preparing interface...", 10, 5),
+    ("Finalizing startup...", 10, 5),
+    ("Almost there...", 5, 5),
+    ("Finishing up...", 15, 5)
+    ];
+
+
+    public async Task LoadApplicationAsync(Action<MainWindowViewModel> onMainWindowReady)
+    {
+        await Task.Yield();
+
+        var assetManager = new AssetManager();
+        var sourceDataManager = new SourceDataManager();
+
+        var assetManagerViewModel = await Task.Run(() => new AssetManagerViewModel(assetManager));
+        var optimizerViewModel = await Task.Run(() => new OptimizerViewModel(assetManager, sourceDataManager));
+        var sourceDataManagerViewModel = await Task.Run(() => new SourceDataManagerViewModel());
+
+        foreach (var (text, steps, delay) in messages)
+        {
+            LoadingText = text;
+            for (int i = 0; i < steps; i++)
+            {
+                Progress++;
+                await Task.Delay(delay);
+            }
+        }
+
+        await Task.Delay(50);
+
+        var mainVM = new MainWindowViewModel(assetManagerViewModel, optimizerViewModel, sourceDataManagerViewModel);
+        onMainWindowReady?.Invoke(mainVM);
+    }
+}
---
+++ Views/LoadingWindow.axaml.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using Sem2Proj.ViewModels;
+using Sem2Proj.Models;
+using Avalonia.Input;
+using System.Runtime.InteropServices;
+
+namespace Sem2Proj.Views;
+
+public partial class LoadingWindow : Window
+{
+    public LoadingWindow()
+    {
+        // Not to be used.
+        // Avalonia won't shut up without this argumentless constructor.
+        InitializeComponent();
+    }
+    public LoadingWindow(LoadingWindowViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+
+        StartLoading();
+    }
+
+    private async void StartLoading()
+    {
+        if (DataContext is LoadingWindowViewModel viewModel)
+        {
+            await viewModel.LoadApplicationAsync(vm =>
+            {
+                var mainWindow = new MainWindow
+                {
+                    DataContext = vm
+                };
+
+                mainWindow.Show();
+                Close();
+            });
+        }
+    }
+
+    private void WindowDragMove(object sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            BeginMoveDrag(e);
+        }
+    }
+}
