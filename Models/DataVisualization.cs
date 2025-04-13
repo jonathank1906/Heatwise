1f4668d271d5b43ddcd5ce9b89714ad56a9635c0
Jonathan Kleffel <jklef24@student.sdu.dk>
2025-04-13 08:15:41 +0200

    Commit before moving code to DV

--- Models/DataVisualization.cs
+++ Models/DataVisualization.cs
@@ -2,32 +2,9 @@ using System;
 using System.Collections.Generic;
 using ScottPlot;
 
-namespace Sem2Proj.Models
+namespace Sem2Proj.Models;
+
+public class DataVisualization
 {
-    public class DataVisualization
-    {
-        // // Generate and save a heat demand plot
-        // public void RenderHeatDemandPlot(double[] xValues, double[] yValues)
-        // {
-        //     var plt = new ScottPlot.Plot(600, 400);
-        //     plt.AddScatter(xValues, yValues, color: System.Drawing.Color.LightBlue, label: "Heat Demand");
-        //     plt.Title("Heat Demand Over Time");
-        //     plt.XLabel("Time");
-        //     plt.YLabel("Heat Demand (MWh)");
-        //     plt.Legend();
-        //     plt.SaveFig("HeatDemandPlot.png");
-        // }
-        
-        // // Generate and save an electricity price plot
-        // public void RenderElectricityPricePlot(double[] xValues, double[] yValues)
-        // {
-        //     var plt = new ScottPlot.Plot(600, 400);
-        //     plt.AddScatter(xValues, yValues, color: System.Drawing.Color.LightGreen, label: "Electricity Price");
-        //     plt.Title("Electricity Price Over Time");
-        //     plt.XLabel("Time");
-        //     plt.YLabel("Electricity Price (DKK/MWh)");
-        //     plt.Legend();
-        //     plt.SaveFig("ElectricityPricePlot.png");
-        // }
-    }
+
 }
--- Models/ResultDataManager.cs
+++ Models/ResultDataManager.cs
@@ -4,42 +4,9 @@ using System.IO;
 using System.Text.Json;
 using Sem2Proj.Models;
 
-namespace Sem2Proj.Models
+namespace Sem2Proj.Models;
+
+public class ResultDataManager
 {
-    public class ResultDataManager
-    {
-        private readonly string _resultsFilePath = "optimization_results.json";
-        
-        // Save optimization result object to JSON
-        public void SaveResults(object results)
-        {
-            try
-            {
-                string jsonString = JsonSerializer.Serialize(results, new JsonSerializerOptions { WriteIndented = true });
-                File.WriteAllText(_resultsFilePath, jsonString);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error saving optimization results: {ex.Message}");
-            }
-        }
-        
-        // Load optimization results from file
-        public T? LoadResults<T>()
-        {
-            try
-            {
-                if (File.Exists(_resultsFilePath))
-                {
-                    string jsonString = File.ReadAllText(_resultsFilePath);
-                    return JsonSerializer.Deserialize<T>(jsonString);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading optimization results: {ex.Message}");
-            }
-            return default;
-        }
-    }
+
 }
